9df4230c40a749a3aad44c350d34ff216690dc3e
Your Name <you@example.com>
2023-03-23 22:50:04 +0800

    add LICENSE.md

--- utils.js
+++ utils.js
@@ -4,12 +4,9 @@ function convertToCamelCase(str) {
 	return str.replace(regex, (match, p1) => {
 		// 将匹配到的字符串转换为驼峰命名法
 		return p1
+			.replace(/^_+|_+$/g, '')
 			.toLowerCase() // 第一个单词首字母小写
 			.replace(/_(\w)/g, (match, p1) => p1.toUpperCase()) // 后面单词首字母大写
 	});
 }
-
-
 exports.convertToCamelCase = convertToCamelCase;
-
-console.log(convertToCamelCase('__unused_webpack_exports'));
