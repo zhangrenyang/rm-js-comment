4a8ec0f05b2c441b4c5062376ef28719f74ac003
Administrator <Administrator@qq.com>
2024-07-14 12:56:31 +0800

    feat(): add readableCode

--- extension.js
+++ extension.js
@@ -1,46 +1,50 @@
 const vscode = require('vscode');
-const babel = require("@babel/core");
-const env = require("@babel/preset-env");
-const jsx = require("@babel/plugin-syntax-jsx");
 const { convertToCamelCase } = require('./utils');
 function activate(context) {
-	const disposable = vscode.commands.registerCommand('extension.removeJsComment', function () {
-		vscode.window.activeTextEditor.edit(editBuilder => {
-			const text = vscode.window.activeTextEditor.document.getText();
-			const options = {
-				plugins:[jsx],
-				comments: false
-			};			
-			try{
-				let { code } = babel.transformSync(text, options);
-				//转换变量
-				code = convertToCamelCase(code);
-				//替换变量
-				const { replacer } = vscode.workspace.getConfiguration('rm-js-comment');
-				for (const key in replacer) {
-					code = code.replaceAll(key, replacer[key]);
-				}
-				//删除空行
-				code = code.replace(/^\s*(?=(\r|$))\n/img, '');
-				const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
-				editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), code);
-				vscode.commands.executeCommand(`editor.action.formatDocument`);
-			}catch(error){
-				console.error(error);
+	const disposableRemoveComment = vscode.commands.registerCommand('extension.removeComment', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+		editor.edit(editBuilder => {
+			let code = editor.document.getText();
+			code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+			code = code.replace(/^\s*[\r\n]/gm, '');
+			const end = new vscode.Position(editor.document.lineCount + 1, 0);
+			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), code);
+			vscode.commands.executeCommand('editor.action.formatDocument');
+		});
+	});
+	context.subscriptions.push(disposableRemoveComment);
+	const disposableReadableCode = vscode.commands.registerCommand('extension.readableCode', function () {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+		editor.edit(editBuilder => {
+			const text = editor.document.getText();
+			let code = text.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+			code = convertToCamelCase(code);
+			const { replacer } = vscode.workspace.getConfiguration('rm-js-comment');
+			for (const key in replacer) {
+				code = code.replaceAll(key, replacer[key]);
 			}
+			code = code.replace(/^\s*[\r\n]/gm, '');
+			const end = new vscode.Position(editor.document.lineCount + 1, 0);
+			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), code);
+			vscode.commands.executeCommand('editor.action.formatDocument');
 		});
 	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableReadableCode);
+
 	const disposableRemoveEmptyLine = vscode.commands.registerCommand('extension.removeEmptyLine', function () {
-		vscode.window.activeTextEditor.edit(editBuilder => {
-			const text = vscode.window.activeTextEditor.document.getText();
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+		editor.edit(editBuilder => {
+			const text = editor.document.getText();
 			const newText = text.replace(/^\s*[\r\n]/gm, '');
-			const end = new vscode.Position(vscode.window.activeTextEditor.document.lineCount + 1, 0);
+			const end = new vscode.Position(editor.document.lineCount + 1, 0);
 			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), newText);
-			vscode.commands.executeCommand(`editor.action.formatDocument`);
+			vscode.commands.executeCommand('editor.action.formatDocument');
 		});
 	});
-	
 	context.subscriptions.push(disposableRemoveEmptyLine);
 }
 
