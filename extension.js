c6f8a58554c30dcc4981611535f6117182763359
Administrator <Administrator@qq.com>
2024-07-14 13:27:27 +0800

    feat(): add removeAllComment

--- extension.js
+++ extension.js
@@ -1,33 +1,74 @@
 const vscode = require('vscode');
-const { convertToCamelCase } = require('./utils');
+const { convertToCamelCase, removeCommentAndEmptyLines } = require('./utils');
+const { promisify } = require('util');
+const fs = require('fs');
+const path = require('path');
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+async function processFile(filePath) {
+	try {
+		let code = await readFile(filePath, 'utf8');
+		await writeFile(filePath, removeCommentAndEmptyLines(code), 'utf8');
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function processDirectory(dirPath) {
+	const entries = await readdir(dirPath, { withFileTypes: true });
+	for (const entry of entries) {
+		const entryPath = path.join(dirPath, entry.name);
+		if (entry.isDirectory()) {
+			await processDirectory(entryPath);
+		} else {
+			await processFile(entryPath);
+		}
+	}
+}
+
+async function processUri(uri) {
+	const uriStat = await stat(uri.fsPath);
+	if (uriStat.isDirectory()) {
+		await processDirectory(uri.fsPath);
+	} else {
+		await processFile(uri.fsPath);
+	}
+}
 function activate(context) {
 	const disposableRemoveComment = vscode.commands.registerCommand('extension.removeComment', function () {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) return;
 		editor.edit(editBuilder => {
 			let code = editor.document.getText();
-			code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
-			code = code.replace(/^\s*[\r\n]/gm, '');
 			const end = new vscode.Position(editor.document.lineCount + 1, 0);
-			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), code);
+			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), removeCommentAndEmptyLines(code));
 			vscode.commands.executeCommand('editor.action.formatDocument');
 		});
 	});
 	context.subscriptions.push(disposableRemoveComment);
+	const disposableRemoveAllComment = vscode.commands.registerCommand('extension.removeAllComment', async function (uri, selectedUris) {
+		let uriList = selectedUris.length > 0 ? selectedUris : [uri];
+		for (const uri of uriList) {
+			await processUri(uri);
+		}
+		vscode.window.showInformationMessage('Comments and empty lines removed from selected files and folders.');
+	});
+
+	context.subscriptions.push(disposableRemoveAllComment);
 	const disposableReadableCode = vscode.commands.registerCommand('extension.readableCode', function () {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) return;
 		editor.edit(editBuilder => {
-			const text = editor.document.getText();
-			let code = text.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+			let code = editor.document.getText();
 			code = convertToCamelCase(code);
 			const { replacer } = vscode.workspace.getConfiguration('rm-js-comment');
 			for (const key in replacer) {
 				code = code.replaceAll(key, replacer[key]);
 			}
-			code = code.replace(/^\s*[\r\n]/gm, '');
 			const end = new vscode.Position(editor.document.lineCount + 1, 0);
-			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), code);
+			editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), removeCommentAndEmptyLines(code));
 			vscode.commands.executeCommand('editor.action.formatDocument');
 		});
 	});
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 function convertToCamelCase(str) {
 	// 匹配所有以___开头和结束的字符串
 	const regex = /(_{2,3})(\w+)\1/g;
-	return str.replace(regex, (_, __,p2) => {
+	return str.replace(regex, (_, __, p2) => {
 		// 将匹配到的字符串转换为驼峰命名法
 		return p2
 			.replace(/^_+|_+$/g, '')
@@ -9,4 +9,10 @@ function convertToCamelCase(str) {
 			.replace(/_(\w)/g, (match, p1) => p1.toUpperCase()) // 后面单词首字母大写
 	});
 }
+function removeCommentAndEmptyLines(code) {
+	code = code.replace(/\/\/.*|\/\*[\s\S]*?\*\//g, '');
+	code = code.replace(/^\s*[\r\n]/gm, '');
+	return code;
+}
 exports.convertToCamelCase = convertToCamelCase;
+exports.removeCommentAndEmptyLines = removeCommentAndEmptyLines;
